1f1788980989aec41575b745587eeda9ca4bcc33
huangshanyong <huangshanyong>
2021-12-01 16:00:56 +0800

    first commit

---
+++ src/HttpRequest.php
@@ -0,0 +1,151 @@
+<?php
+
+
+namespace Wtf10029\Getui;
+
+
+use GuzzleHttp\Client;
+
+/**
+ * Class HttpRequest
+ * @package Getui
+ */
+class HttpRequest
+{
+    /**
+     * @var string
+     */
+    protected $method = 'GET';
+    /**
+     * @var array
+     */
+    protected $headers = [
+        'Content-Type' => 'application/json;charset=utf-8'
+    ];
+    /**
+     * @var Client
+     */
+    protected $client;
+    /**
+     * 接口网关
+     * @var string
+     */
+    protected $gateway = 'https://restapi.getui.com/v2/';
+    /**
+     * 配置
+     * @var Config
+     */
+    protected $config;
+    /**
+     * @var array
+     */
+    protected $data = [];
+    /**
+     * @var string
+     */
+    protected $api;
+    /**
+     * @var Authorization
+     */
+    protected $authorization;
+
+    /**
+     * HttpRequest constructor.
+     */
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    /**
+     * 设置请求方法
+     * @param string $method
+     * @return $this
+     */
+    public function withMethod(string $method): self
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function withData(array $data): self
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    /**
+     * 设置Token
+     * @param Authorization $authorization
+     * @return $this
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function withToken(Authorization $authorization)
+    {
+        $this->authorization = $authorization;
+        return $this;
+    }
+
+    /**
+     * 设置配置
+     * @param Config $config
+     * @return $this
+     */
+    public function withConfig(Config $config)
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    /**
+     * @param string $api
+     * @return $this
+     */
+    public function withApi(string $api)
+    {
+        $this->api = $api;
+        return $this;
+    }
+
+    /**
+     * 发起请求
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function send()
+    {
+        try {
+            $this->data['appkey'] = $this->config->getAppKey();
+            $this->data['timestamp'] = $this->micro_time();
+            $this->data['sign'] = hash("sha256",
+                "{$this->config->getAppKey()}{$this->data['timestamp']}{$this->config->getMasterSecret()}");
+            if ($this->authorization instanceof Authorization && method_exists($this->authorization,
+                    'getTokenAsString')) {
+                $this->headers['token'] = $this->authorization->getTokenAsString();
+            }
+            return json_decode($this->client->request($this->method,
+                $this->gateway . $this->config->getAppId() . $this->api, [
+                    'headers' => $this->headers,
+                    'body'    => json_encode($this->data)
+                ])->getBody()->getContents(), 1);
+        } catch (\GuzzleHttp\Exception\ClientException $exception) {
+            $data = json_encode($exception->getResponse()->getBody()->getContents(), 1);
+            if ($exception->getResponse()->getStatusCode() == 400 && $data['code'] == 20001) {
+                $this->authorization->refurbishToken();
+                $this->send();
+            }
+            throw $exception;
+        }
+    }
+
+    private function micro_time()
+    {
+        list($usec, $sec) = explode(" ", microtime());
+        $time = ($sec . substr($usec, 2, 3));
+        return $time;
+    }
+}
---
+++ src/Push.php
@@ -0,0 +1,97 @@
+<?php
+
+
+namespace Wtf10029\Getui;
+
+use Getui\Message\MessageInterface;
+use Getui\Message\NotificationMessage;
+
+/**
+ * Class Push
+ * @package Getui
+ */
+class Push
+{
+    use ApiTrait;
+
+    /**
+     * 单推 Client ID
+     * @param array $cid
+     * @param NotificationMessage $message
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function toSingleCid(array $cid, NotificationMessage $message)
+    {
+        $data = (new HttpRequest())->
+        withApi('/push/single/cid')->
+        withMethod('POST')->
+        withConfig($this->config)->
+        withToken($this->auth)->
+        withData([
+            'request_id'   => rand(11111100000, 9999999900009),
+            'settings'     => [
+                'ttl' => 3600000
+            ],
+            'audience'     => [
+                'cid' => $cid
+            ],
+            'push_message' => $message->toArray()
+        ])->
+        send();
+        if (!(isset($data['msg']) && $data['msg'] === 'success' && isset($data['code']) && $data['code'] === 0)) {
+            throw new \Exception(isset($data['msg']) ? $data['msg'] : '接口错误');
+        }
+        return $data;
+    }
+
+    /**
+     * 推送消息到所有用户
+     * @param NotificationMessage $message
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function toAll(NotificationMessage $message)
+    {
+        return (new HttpRequest())->
+        withApi('/push/all')->
+        withMethod('POST')->
+        withConfig($this->config)->
+        withToken($this->auth)->
+        withData([
+            'request_id'   => rand(11111100000, 9999999900009),
+//            'group_name'=>'',
+            'settings'     => [
+                'ttl' => 3600000
+            ],
+            'audience'     => 'all',
+            'push_message' => $message->toArray()
+        ])->
+        send();
+    }
+
+    /**
+     * 单推 Alias
+     * @param array $alias
+     * @param NotificationMessage $message
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function toSingleAlias(array $alias, NotificationMessage $message)
+    {
+        return (new HttpRequest())->
+        withApi('/push/single/alias')->
+        withMethod('POST')->
+        withConfig($this->config)->
+        withToken($this->auth)->
+        withData([
+            'audience'     => [
+                'alias' => $alias
+            ],
+            'request_id'   => rand(11111100000, 9999999900009),
+            'push_message' => $message->toArray()
+        ])->
+        send();
+    }
+
+}
